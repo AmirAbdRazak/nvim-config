f74a696238fab241864c808a2278e0a0d99a71f1
Amir Razak <amrrzk02@gmail.com>
2024-05-12 00:06:27 +0800

    update

--- init.lua
+++ init.lua
@@ -77,6 +77,7 @@ require('lazy').setup({
   'windwp/nvim-autopairs',
   'airblade/vim-gitgutter',
   'wellle/context.vim',
+  'Olical/conjure',
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
   -- Colorizer
